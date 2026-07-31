a9d2767ece071fef232e91d618a4de0bec093feb
Charlie Windolf <charliexwindolf@gmail.com>
2026-07-31 14:00:54 -0400

    remove unused file

--- src/dartsort/clustering/__init__.py
+++ src/dartsort/clustering/__init__.py
@@ -1,3 +1,8 @@
-from .clustering_features import SimpleMatrixFeatures, StableWaveformFeatures
-from .clustering import get_clusterer, clustering_strategies, refinement_strategies, TMMRefinement
 from .agglomerate import deduplicate_spikes
+from .clustering import (
+    TMMRefinement,
+    clustering_strategies,
+    get_clusterer,
+    refinement_strategies,
+)
+from .clustering_features import SimpleMatrixFeatures, StableWaveformFeatures
